9418c0e34da03d7b441f2d7662a8565bd5f35a5f
jm-olave <jm.olave@uniandes.edu.co>
2023-05-24 16:49:49 -0500

    cambios

---
+++ hola.py
@@ -0,0 +1,26 @@
+hola = "Hola Mundo manzana"
+a = 1.5
+b = 1.5
+# valor = int(input("valor:"))
+# print(a+b,type(valor) )
+lista2 = [ 3,5]
+
+tupla = (("texto",2),(True, lista2))
+newdict = dict(tupla)
+print(newdict)
+lista = [2,"texto",True, hola, lista2]
+lista.append(a)
+# print(lista)
+
+
+conjunto = {3, 5, True, 3.4}
+dicccionario = {
+    "manzana": {
+        "semilla": 2,
+        "color": "verde"
+    },
+    "mango": True,
+    "banano": lista2
+}
+dicccionario
+print(dicccionario)
